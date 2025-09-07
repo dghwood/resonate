6553f7939c227be6469e631f532e1a060b2937ae
Duncan <dgh.wood@gmail.com>
2025-09-06 22:12:38 -0400

    Adding repeated fields support to proto utils

--- app/lib/utils/proto.dart
+++ app/lib/utils/proto.dart
@@ -24,13 +24,17 @@ class DatabaseProtoStoreUtils<T extends GeneratedMessage> {
     return latin1.decode(message.writeToBuffer());
   }
 
-  void messageFromString(String storeMessage, GeneratedMessage message) {
+  void messageFromString<K extends GeneratedMessage>(
+    String storeMessage,
+    K message,
+  ) {
     message.mergeFromBuffer(latin1.encode(storeMessage));
   }
 
   DatabaseStoreType toStore() {
     final Map<String, Object> storeMap = {};
     for (var field in _descriptor.field) {
+      bool isRepeated = false;
       if (!_message.hasField(field.number)) {
         continue; // Skip fields that are not set
       }
@@ -39,22 +43,35 @@ class DatabaseProtoStoreUtils<T extends GeneratedMessage> {
             (_message.getField(field.number) as List).isEmpty) {
           continue; // Skip fields that are empty arrays
         }
-        throw UnimplementedError(
-          'Repeated field handling not implemented for field: ${field.name}',
-        );
+        isRepeated = true;
+        // throw UnimplementedError(
+        //   'Repeated field handling not implemented for field: ${field.name}',
+        // );
       }
       var storeFieldName = 'field_${field.number}';
       switch (field.type) {
         case FieldDescriptorProto_Type.TYPE_INT64:
           storeMap[storeFieldName] =
-              (_message.getField(field.number) as Int64).toInt();
+              isRepeated
+                  ? (_message.getField(field.number) as Iterable<Int64>)
+                      .map((i) => i.toInt())
+                      .toList()
+                  : (_message.getField(field.number) as Int64).toInt();
+
         case FieldDescriptorProto_Type.TYPE_STRING:
         case FieldDescriptorProto_Type.TYPE_BOOL:
           storeMap[storeFieldName] = _message.getField(field.number);
           break;
         case FieldDescriptorProto_Type.TYPE_MESSAGE:
-          var message = _message.getField(field.number) as GeneratedMessage;
-          storeMap[storeFieldName] = messageToString(message);
+          if (!isRepeated) {
+            var message = _message.getField(field.number) as GeneratedMessage;
+            storeMap[storeFieldName] = messageToString(message);
+          } else {
+            var messages =
+                _message.getField(field.number) as Iterable<GeneratedMessage>;
+            storeMap[storeFieldName] =
+                messages.map((i) => messageToString(i)).toList();
+          }
         default:
           throw UnimplementedError(
             'toStore handling not implemented for field: ${field.name}',
@@ -71,20 +88,48 @@ class DatabaseProtoStoreUtils<T extends GeneratedMessage> {
         continue; // Skip fields that are not in the store map
       }
       var value = storeMap[storeFieldName]!;
-
+      var isRepeated = field.label == FieldDescriptorProto_Label.LABEL_REPEATED;
       switch (field.type) {
         case FieldDescriptorProto_Type.TYPE_INT64:
-          _message.setField(field.number, Int64(value as int));
+          if (!isRepeated) {
+            _message.setField(field.number, Int64(value as int));
+          } else {
+            _message.setField(
+              field.number,
+              (value as List<int>).map((i) => Int64(i)),
+            );
+          }
         case FieldDescriptorProto_Type.TYPE_STRING:
         case FieldDescriptorProto_Type.TYPE_BOOL:
           _message.setField(field.number, value);
         case FieldDescriptorProto_Type.TYPE_MESSAGE:
-          var subMessage = _message.getField(field.number) as GeneratedMessage;
-          if (!_message.hasField(field.number)) {
-            subMessage = subMessage.createEmptyInstance();
+          var builder = _message.info_.fieldInfo[field.number]!.subBuilder!;
+          if (!isRepeated) {
+            // var subMessage =
+            //     _message.getField(field.number) as GeneratedMessage;
+            // if (!_message.hasField(field.number)) {
+            //   subMessage = subMessage.createEmptyInstance();
+            //   _message.setField(field.number, subMessage);
+            // }
+            var subMessage = builder();
+            messageFromString(value as String, subMessage);
             _message.setField(field.number, subMessage);
+          } else {
+            var repeatedField =
+                _message.getField(field.number) as List<GeneratedMessage>;
+            var values =
+                (value as List<String>).map((i) {
+                  var m = builder();
+                  messageFromString(i, m);
+                  return m;
+                }).toList();
+            // For some reason I need to add these individually, since addAll
+            // seems to not be able to identify this as the right message
+            // and throws an error.
+            for (var m in values) {
+              repeatedField.add(m);
+            }
           }
-          messageFromString(value as String, subMessage);
         default:
           throw UnimplementedError(
             'fromStore handling not implemented for field: ${field.name}',
--- app/test/utils/proto_test.dart
+++ app/test/utils/proto_test.dart
@@ -1,11 +1,58 @@
+import 'dart:typed_data';
+
 import 'package:fixnum/fixnum.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:protobuf/protobuf.dart';
+import 'package:protobuf_google/protobuf_google.dart';
 import 'package:resonate/proto/common.pb.dart';
 import 'package:resonate/proto/common.pbjson.dart';
 import 'package:resonate/utils/proto.dart';
 
+// class ProtoUtil<T extends GeneratedMessage> {
+//   ProtoUtil(this.message, this.descriptor);
+//   final T message;
+//   final DescriptorProto descriptor;
+
+//   void fromStore(List<Uint8List> store) {
+//     for (var field in descriptor.field) {
+//       if (field.label == FieldDescriptorProto_Label.LABEL_REPEATED) {
+//         var repeatedField =
+//             message.getField(field.number) as List<GeneratedMessage>;
+//         var builder = message.info_.fieldInfo[field.number]!.subBuilder!;
+//         repeatedField.addAll(
+//           store.map((i) {
+//             var m = builder();
+//             m.mergeFromBuffer(i);
+//             return m;
+//           }),
+//         );
+//       }
+//       break;
+//     }
+//   }
+// }
+
 void main() {
   group('DatabaseProtoStoreUtils Model Tests', () {
+    // test('nestedDescriptors', () {
+    //   var message = UserFeedMessage();
+    //   var descriptor = DescriptorProto.fromBuffer(userFeedMessageDescriptor);
+    //   var store = [
+    //     UserFeedItemMessage(
+    //       episodeItem: UserFeedItemEpisodeMessage(
+    //         episode: EpisodeMessage(id: '1'),
+    //       ),
+    //     ).writeToBuffer(),
+    //     UserFeedItemMessage(
+    //       episodeItem: UserFeedItemEpisodeMessage(
+    //         episode: EpisodeMessage(id: '2'),
+    //       ),
+    //     ).writeToBuffer(),
+    //   ];
+
+    //   var u = ProtoUtil(message, descriptor);
+    //   u.fromStore(store);
+    // });
     test(
       'toStore should correctly convert UserSubscriptionMessage to a Map',
       () {
@@ -45,5 +92,35 @@ void main() {
         expect(newMessage.metadata.createdTimestamp, Int64(2));
       },
     );
+
+    test('toStore should correctly convert UserFeedMessage to a Map', () {
+      var message = UserFeedMessage(
+        items: [
+          UserFeedItemMessage(
+            episodeItem: UserFeedItemEpisodeMessage(
+              episode: EpisodeMessage(id: '1'),
+            ),
+          ),
+          UserFeedItemMessage(
+            episodeItem: UserFeedItemEpisodeMessage(
+              episode: EpisodeMessage(id: '2'),
+            ),
+          ),
+        ],
+      );
+
+      var writeStore =
+          DatabaseProtoStoreUtils(userFeedMessageDescriptor, message).toStore();
+
+      var newMessage = UserFeedMessage();
+      var readStore = DatabaseProtoStoreUtils(
+        userFeedMessageDescriptor,
+        newMessage,
+      ).fromStore(writeStore);
+
+      expect(newMessage.items.length, 2);
+      expect(newMessage.items[0].episodeItem.episode.id, '1');
+      expect(newMessage.items[1].episodeItem.episode.id, '2');
+    });
   });
 }
