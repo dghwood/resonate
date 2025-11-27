1cb9a93418aac2837c0c347a4dd101d968fc0715
duncan <dgh.wood@gmail.com>
2025-11-26 23:01:28 -0500

    Other changes..?

--- app/deploy.sh
+++ app/deploy.sh
@@ -7,9 +7,11 @@ set -e
 flutter build web --$1
 
 # upload to storage 
-gcloud storage rsync ../app/build/web gs://rxyz-app-static/v0 --recursive
-
+# gcloud storage rsync ../app/build/web gs://rxyz-app-static/v0 --recursive
 # I ended up doing this in the UI actually.
 # gcloud storage buckets add-iam-policy-binding  gs://rxyz-app-static --member=allUsers --role=roles/storage.objectViewer
 # This needs to be changed.. 
 # gcloud storage buckets update gs://rxyz-app-static --cors-file=cors.cfg
+
+# Use firebase hosting (not app hosting)
+firebase deploy --only hosting
