bb1fa85a32e161174be7579062b1ab64038da553
duncan <dgh.wood@gmail.com>
2026-01-22 19:56:04 -0500

    Getting android dev environment to trust my local https cert.

--- app/lib/services/http/http.dart
+++ app/lib/services/http/http.dart
@@ -5,6 +5,8 @@ import 'package:logging/logging.dart';
 import 'package:resonate/errors/errors.dart';
 import 'package:resonate/services/http/cookie_jar.dart';
 import 'package:resonate/services/secure_database/secure_database.dart';
+import 'package:cronet_http/cronet_http.dart';
+import 'package:universal_io/io.dart';
 
 Logger _log = Logger('services/http');
 
@@ -20,8 +22,23 @@ class HttpService implements AbstractHttpService {
   HttpService({required AbstractSecureDatabase secureDatabse})
     : cookieJar = BetterPersistCookieJar(
         storage: SecureCookieStorage(secureDatabase: secureDatabse),
+      ) {
+    if (Platform.isAndroid & kDebugMode) {
+      // https://github.com/dart-lang/http/issues/458
+      // Dart doesn't work well will custom certificates
+      final engine = CronetEngine.build(
+        cacheMode: CacheMode.memory,
+        cacheMaxSize: 2 * 1024 * 1024,
+        userAgent: 'resonates.xyz',
       );
+      _client = CronetClient.fromCronetEngine(engine, closeEngine: true);
+    } else {
+      // The default for release.
+      _client = http.Client();
+    }
+  }
 
+  late final http.Client _client;
   final BetterPersistCookieJar cookieJar;
 
   @override
@@ -31,13 +48,14 @@ class HttpService implements AbstractHttpService {
     Uint8List? body,
   }) async {
     headers ??= {};
+    headers['User-Agent'] = 'resonates.xyz';
     headers['Content-Type'] = 'application/octet-stream';
     try {
       if (!kIsWeb) {
         await cookieJar.actuallyLoadForRequest(headers, url);
       }
       _log.info('headers::$headers');
-      var response = await http.post(url, headers: headers, body: body);
+      var response = await _client.post(url, headers: headers, body: body);
       if (response.statusCode != 200) {
         throw HttpServiceException(
           'Failed to post to $url: ${response.statusCode} ${response.reasonPhrase}',
